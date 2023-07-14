a897242bf65a4d3749369c231986534aacee1026
PaulRojasUN <paul.rojas@correounivalle.edu.co>
2023-07-14 11:44:56 -0500

    Added rest of the entities models

--- app/main_app/models.py
+++ app/main_app/models.py
@@ -5,7 +5,7 @@
 
 
 from django.db import models
-
+from django.contrib.auth.models import User
 
 ### Tag ###
 
@@ -14,22 +14,35 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name;
+ 
+class ClassifiedTag(models.Model):
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
 
+class UnclassifiedTag(models.Model):
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
 
 class TagRecipe(models.Model):
-    tag_id = models.ForeignKey('Tag', related_name='main_app_tag_recipe_tag_id', on_delete=models.CASCADE);
-    recipe_id = models.ForeignKey('Recipe', related_name='main_app_tag_recipe_recipe_id', on_delete=models.CASCADE);
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
+    recipe_id = models.ForeignKey('Recipe', on_delete=models.CASCADE);
 
     def __str__(self):
         return "t:" + str(self.tag_id) + " - " + "r:" +  str(self.recipe_id);
 
 
 class TagIngredient(models.Model):
-    tag_id = models.ForeignKey('Tag', related_name='main_app_tag_ingredients_tag_id', on_delete=models.CASCADE);
-    ingredient_id = models.ForeignKey('Ingredient', related_name='main_app_tag_ingredients_ingredient_id', on_delete=models.CASCADE);
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
+    ingredient_id = models.ForeignKey('Ingredient', on_delete=models.CASCADE);
+
+class TagPost(models.Model):
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
+    post_id = models.ForeignKey('Post', on_delete=models.CASCADE);
+
+class TagUser(models.Model):
+    tag_id = models.ForeignKey('Tag', on_delete=models.CASCADE);
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE);
 
 
-### --- ###
+### ////////////// ###
 
 
 ### Recipe ###
@@ -40,7 +53,14 @@ class Recipe(models.Model):
     def __str__(self):
         return self.name;
 
-### --- ###
+class RecipeIngredients(models.Model):
+    ingredient_id = models.ForeignKey('Ingredient', on_delete=models.CASCADE);
+    recipe_id = models.ForeignKey('Recipe', on_delete=models.CASCADE);
+
+### ////////////// ###
+
+
+
 
 ### Ingredient ###
 
@@ -48,14 +68,33 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=64);
 
 class ClassifiedIngredient(models.Model):
-    ingredient_id = models.ForeignKey('Ingredient', related_name='main_app_classified_ingredient_id', on_delete=models.CASCADE);
+    ingredient_id = models.ForeignKey('Ingredient', on_delete=models.CASCADE);
 
 class UnclassifiedIngredient(models.Model):
-    ingredient_id = models.ForeignKey('Ingredient', related_name='main_app_unclassified_ingredient_id', on_delete=models.CASCADE);
-### --- ###
+    ingredient_id = models.ForeignKey('Ingredient', on_delete=models.CASCADE);
+
+### ////////////// ###
+
+
 
 
 ### Post ### 
 
-### --- ###
+class Post(models.Model):
+    author_user_id = models.ForeignKey(User, on_delete=models.CASCADE);
+    recipe_id = models.ForeignKey('Recipe', on_delete=models.CASCADE);
 
+class PostLike(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE);
+    post_id = models.ForeignKey('Post', on_delete=models.CASCADE);
+    
+### ////////////// ###
+
+
+
+### User ### 
+
+class FollowingUser(models.Model):
+    follower_user_id = models.ForeignKey(User, related_name='main_app_following_user_follower_user', on_delete=models.CASCADE);
+    target_user_id = models.ForeignKey(User, related_name='main_app_following_user_target_user', on_delete=models.CASCADE);
+### ////////////// ###
