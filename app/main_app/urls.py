156b10c1e58714d2a5438638831d5fb35e748f8b
PaulRojasUN <paul.rojas@correounivalle.edu.co>
2023-07-15 16:51:46 -0500

    Added basic endpoints

--- app/main_app/urls.py
+++ app/main_app/urls.py
@@ -3,8 +3,15 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
+    path('create_recipe/', views.create_recipe, name='create_recipe'),
+    path('edit_recipe/<int:id>/', views.edit_recipe, name='edit_recipe'),
+    path('view_recipe/<int:id>/', views.view_recipe, name='view_recipe'),
+    path('view_account/<int:id>/', views.view_account, name='view_account'),
+    path('edit_account/<int:id>/', views.edit_account , name='edit_account'),
+    path('social/<int:id>/', views.social, name='social'),
+    path('filter/', views.filter, name='filter'),
 ]
--- app/main_app/views.py
+++ app/main_app/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+###
+from django.http import HttpResponse
+
+
+### Registration ###
 
 @login_required
 def home(request):
-    return render(request, "registration/success.html", {})
+    name = "";
+    try:
+        name = request.user.username;
+        print(name)
+    except Exception as e:
+        print(e);
+    return render(request, "registration/home_page.html", {name:name})
  
 def register(request):
     if request.method == 'POST':
@@ -21,3 +31,56 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+###  ///////////////////////// ###
+
+
+def create_recipe(request):
+    if request.method == 'GET':
+        return HttpResponse('create_recipe');
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def edit_recipe(request, id):
+    if request.method == 'GET':
+        return HttpResponse('edit recipe' + str(id));
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def view_recipe(request, id):
+    if request.method == 'GET':
+        return HttpResponse('view recipe' + str(id));
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def view_account(request, id):
+    if request.method == 'GET':
+        return HttpResponse('view account' + str(id));
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def edit_account(request, id):
+    if request.method == 'GET':
+        return HttpResponse('edit account' + str(id));
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def social(request, id):
+    if request.method == 'GET':
+        return HttpResponse('social' + str(id));
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
+def filter(request):
+    if request.method == 'GET':
+        
+        obj = request.GET;
+
+        for i in obj:
+            print(str(i) + ":" + str(obj[i]));
+            
+        return HttpResponse('filter');
+    else:
+        return HttpResponse('Unsupported method', status=405);
+
